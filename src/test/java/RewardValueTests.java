d110daffd7857b89684dcef36a8651658d960400
vanshikak04 <vanshika.khanna6@gmail.com>
2024-10-02 21:59:42 +0530

    Completed RewardValue unit tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,30 +1,39 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RewardValueTests {
 
     @Test
-    void create_with_cash_value() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
+    public void testCashToMilesConversion() {
+        // Given a cash value of 100
+        RewardValue rewardValue = new RewardValue(100.0);
+        // Expected miles value = 100 / 0.0035
+        double expectedMilesValue = 100.0 / 0.0035;
+        assertEquals(expectedMilesValue, rewardValue.getMilesValue(), 0.0001);
     }
 
     @Test
-    void create_with_miles_value() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
+    public void testMilesToCashConversion() {
+        // Given a miles value of 10000
+        RewardValue rewardValue = new RewardValue(10000);
+        // Expected cash value = 10000 * 0.0035
+        double expectedCashValue = 10000 * 0.0035;
+        assertEquals(expectedCashValue, rewardValue.getCashValue(), 0.0001);
     }
 
     @Test
-    void convert_from_cash_to_miles() {
-        assert false;
+    public void testGetCashValue() {
+        // Given a cash value of 50
+        RewardValue rewardValue = new RewardValue(50.0);
+        // Expected cash value should be exactly 50
+        assertEquals(50.0, rewardValue.getCashValue(), 0.0001);
     }
 
     @Test
-    void convert_from_miles_to_cash() {
-        assert false;
+    public void testGetMilesValue() {
+        // Given a miles value of 5000
+        RewardValue rewardValue = new RewardValue(5000);
+        // Expected miles value should be exactly 5000
+        assertEquals(5000, rewardValue.getMilesValue(), 0.0001);
     }
 }
